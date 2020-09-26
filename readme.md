27f2bdbe95d355419758e85ade122a18a73370d3
nehahirve <neha.s.hirve@gmail.com>
2020-09-26 21:36:31 +0200

    refactored code

--- readme.md
+++ readme.md
@@ -1,2 +1,6 @@
-GEOMETRY: 
+Audio visualiser using the web audio API and the AudioAnalyser node. 
 
+What I learnt:
+
+    canvas API
+    web audio API
--- script.js
+++ script.js
@@ -1,88 +1,144 @@
-let scene, camera, renderer, cube, point, plane
+/*
+*********************************
+CONSTANTS
+*********************************
+*/
+
+const colourPalette = {
+  light: '#FFA500',
+  medium: '#FF8C00',
+  dark: '#FF4500',
+  complement: '#ffc800',
+  accent: '#FAF0E6'
+}
+
+const canvas = document.querySelector('canvas')
+const ctx = canvas.getContext('2d')
+const startBtn = document.querySelector('.play')
+
+let WIDTH = canvas.width
+let HEIGHT = canvas.height
+
+let audioctx, source, audio, analyser, freqs, radius, dashIntervals
+
+let isRunning = false
+
+/*
+*********************************
+HELPER FUNCTIONS
+*********************************
+*/
+
+function drawRing (ctx, radius, weight, stroke) {
+  console.log(stroke)
+  ctx.beginPath()
+  ctx.strokeStyle = stroke
+  ctx.lineWidth = weight
+  ctx.arc(WIDTH / 2, HEIGHT / 2, radius, 0, 2 * Math.PI)
+  ctx.stroke()
+  ctx.closePath()
+}
+
+function center (el, halfsize) {
+  el.style.top = `${(HEIGHT / 2) - (halfsize)}px`
+  el.style.left = `${(WIDTH / 2) - (halfsize)}px`
+}
+
+/*
+*********************************
+ON LOAD
+*********************************
+*/
+
+resizeCanvas()
+startBtn.addEventListener('click', init)
+window.addEventListener('resize', resizeCanvas)
+
+/*
+*********************************
+MAIN FUNCTIONS
+*********************************
+*/
 
 function init () {
-  scene = new THREE.Scene() // new scene
-  camera = new THREE.PerspectiveCamera( // new camera
-    125,
-    window.innerWidth / window.innerHeight,
-    0.1,
-    1000
-  )
+  isRunning = true
+  startBtn.remove()
+  audioctx = new (
+    window.AudioContext || window.webkitAudioContext
+  )() // new audio context
+  audio = new Audio()
+  audio.src = 'media/sample.mp3'
 
-  camera.position.z = 10
+  source = audioctx.createMediaElementSource(audio)
+  analyser = audioctx.createAnalyser()
+  source.connect(analyser)
+  analyser.connect(audioctx.destination)
 
-  renderer = new THREE.WebGLRenderer({ antialias: true }) // new renderer
+  freqs = new Uint8Array(analyser.frequencyBinCount)
 
-  renderer.setSize(window.innerWidth, window.innerHeight) // set render viewport
-  renderer.setPixelRatio(window.devicePixelRatio)
-
-  document.body.appendChild(renderer.domElement) // append render cavnas
-
-  const geometry = new THREE.BoxGeometry(1, 1, 1)
-  const material = new THREE.MeshLambertMaterial({ color: 0x23fba0})
-  cube = new THREE.Mesh(geometry, material)
-
-  scene.add(cube)
-
-  const ambient = new THREE.AmbientLight(0x404040, 1)
-  scene.add(ambient)
-
-  point = new THREE.PointLight(0x404040, 3)
-  point.position.z = 10
-  scene.add(point)
-
-  var geometry2 = new THREE.PlaneGeometry( 200, 200, 32, 32);
-  var material2 = new THREE.MeshLambertMaterial( {color: 0x1ffafa, side: THREE.DoubleSide} )
-  
-  plane = new THREE.Mesh( geometry2, material2 )
-  //scene.add( plane )
-
-  plane.rotation.x = 1
-  plane.position.y = -10
-
-  var size = 500;
-  var divisions = 50;
-  
-  var gridHelper = new THREE.GridHelper( size, divisions, 'magenta', 'magenta' );
-  scene.add( gridHelper )
-
-  gridHelper.position.y = -25
-
-
-  var params = {
-    exposure: 1,
-    bloomStrength: 1.5,
-    bloomThreshold: 0,
-    bloomRadius: 0
-  };
-
-  let bloomPass = new THREE.UnrealBloomPass( new THREE.Vector2( window.innerWidth, window.innerHeight ), 1.5, 0.4, 0.85 );
-			bloomPass.threshold = params.bloomThreshold;
-			bloomPass.strength = params.bloomStrength;
-			bloomPass.radius = params.bloomRadius;
-
-			composer = new THREE.EffectComposer( renderer );
-			composer.addPass( renderScene );
-			composer.addPass( bloomPass );
-
-
+  dashIntervals = [5, 5, 10, 15, 25, 20, 35, 10, 50]
+  audio.play()
+  draw()
 }
 
-function animate () {
-  cube.rotation.x += 0.01 // animation parameters
-  cube.rotation.y += 0.01
-  cube.position.z += 0.01
-  renderer.render(scene, camera) // redraw
-  requestAnimationFrame(animate)
+function draw () {
+  ctx.fillRect(0, 0, canvas.width, canvas.height)
+
+  const bars = 200
+  radius = WIDTH > 500 ? 150 : 100
+  drawInnerRings(ctx, radius)
+  console.log(radius)
+
+  ctx.lineWidth = 4
+
+  ctx.setLineDash(dashIntervals)
+  analyser.getByteFrequencyData(freqs)
+
+  for (let i = 0; i < bars; i++) {
+    const radians = (Math.PI * 2) / bars
+    let barHeight = freqs[i] * 1
+    if (barHeight > canvas.width / 3) barHeight = freqs[i] * 0.3
+
+    const color = `rgb(${255}, ${255 - freqs[i]}, ${0})`
+    const color2 = `rgb(${255}, ${200 - freqs[i]}, ${0})`
+
+    const x = (WIDTH / 2) + Math.cos(radians * i) * radius
+    const xEnd = (WIDTH / 2) + Math.cos(radians * i) * (radius + barHeight)
+    const y = (HEIGHT / 2) + Math.sin(radians * i) * radius
+    const yEnd = (HEIGHT / 2) + Math.sin(radians * i) * (radius + barHeight)
+
+    ctx.strokeStyle = color
+    ctx.fillStyle = color2
+
+    ctx.beginPath()
+    ctx.moveTo(x, y)
+    ctx.lineTo(xEnd, yEnd)
+    ctx.stroke()
+  }
+
+  requestAnimationFrame(draw)
 }
 
-
-init() // initialiaze scene
-animate() // animate
-window.addEventListener('resize', onResize, false) // resizes renderer and aspect ratio
-
-function onResize () {
-  camera.aspect = window.innerWidth / window.innerHeight
-  camera.updateProjectionMatrix() // since we updated aspect
-  renderer.setSize(window.innerWidth, window.innerHeight)
+function resizeCanvas () {
+  canvas.width = window.innerWidth
+  WIDTH = canvas.width
+  canvas.height = window.innerHeight
+  HEIGHT = canvas.height
+  ctx.fillStyle = colourPalette.light
+  ctx.fillRect(0, 0, window.innerWidth, window.innerHeight)
+  if (!isRunning) {
+    center(startBtn, 75)
+  }
 }
+
+function drawInnerRings (ctx, radius) {
+  ctx.setLineDash([])
+  drawRing(ctx, radius - 20, 4, colourPalette.complement)
+  drawRing(ctx, radius - 30, 2, colourPalette.light)
+  drawRing(ctx, radius - 40, 4, colourPalette.medium)
+  drawRing(ctx, radius - 55, 4, colourPalette.medium)
+  drawRing(ctx, radius - 70, 7, colourPalette.complement)
+  drawRing(ctx, radius - 85, 1, colourPalette.dark)
+  drawRing(ctx, radius - 90, 1, colourPalette.medium)
+}
+
